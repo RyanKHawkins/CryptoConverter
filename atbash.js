7a625abc34bbbf2fa9c34e39caa3af6880cdbe6a
Ryan Hawkins <rkhawkins80@hotmail.com>
2020-03-30 00:09:32 +0000

    Updated Atbash cipher for lower and uppercase options.

--- atbash.js
+++ atbash.js
@@ -1,15 +1,21 @@
 //JS Atbash Cipher
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const lowercase_alphabet = [];
+for (let i = 0; i < alphabet.length; i++) {
+    lowercase_alphabet.push(alphabet[i].toLowerCase());
+}
 
 export function atbash(string) {
-    var string = string.toUpperCase();
     var encryptedString = "";
 
     for (var i = 0; i < string.length; i++) {
         if (alphabet.includes(string[i])) {
             var index = alphabet.indexOf(string[i]);
             encryptedString += alphabet.reverse()[index];
+        } else if (lowercase_alphabet.includes(string[i])) {
+            index = lowercase_alphabet.indexOf(string[i]);
+            encryptedString += lowercase_alphabet.reverse()[index];
         } else {
             encryptedString += string[i];
         }
